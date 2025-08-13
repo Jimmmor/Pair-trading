092511ed818a0daff82283ea73a562971eb467b0
Jimmmor <136963506+Jimmmor@users.noreply.github.com>
2025-08-13 14:35:40 +0200

    Update app4+ai.py

--- app4+ai.py
+++ app4+ai.py
@@ -148,13 +148,13 @@ class MLPairsTradingSystem:
         zscore = (spread - rolling_mean) / rolling_std
     
         # --- Return DataFrame with results ---
-        df = pd.DataFrame({
-            'price1': price1,
-            'price2': price2,
-            'spread': spread,
-            'zscore': zscore
-        })
-    
+       df = pd.DataFrame({
+    'price1': price1.values,
+    'price2': price2.values,
+    'spread': spread.values,
+    'zscore': zscore.values
+}, index=price1.index)
+
         return df, hedge_ratio
 
 
